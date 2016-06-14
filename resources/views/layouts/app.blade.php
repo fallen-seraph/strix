84a02b413caee484d10989732ccf834179fa38ab
Aaron <fallenseriphan@gmail.com>
2016-06-14 17:18:04 -0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,15 +40,21 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
+                    Server Strix
                 </a>
             </div>
 
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>
+                @if (Auth::guest())
+                    <ul class="nav navbar-nav">
+                        <li><a href="{{ url('/') }}">Home</a></li>
+                    </ul>
+                @else
+                    <ul class="nav navbar-nav">
+                        <li><a href="{{ url('/monitoring') }}">Home</a></li>
+                    </ul>
+                @endif
 
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
