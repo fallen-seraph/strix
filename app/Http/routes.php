89897d2bbf350529b1bc53c7a50a370486f6243c
Aaron <fallenseriphan@gmail.com>
2016-06-14 13:18:50 -0600

    Update routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,8 +20,8 @@ Route::post('login', 'Auth\AuthController@login');
 Route::get('logout', 'Auth\AuthController@logout');
 
 // Registration Routes...
-Route::get('register', 'Auth\RegisterController@showForm');
-Route::post('register', 'Auth\RegisterController@register');
+Route::get('register', 'RegisterController@showForm');
+Route::post('register', 'RegisterController@register');
 
 // Password Reset Routes...
 Route::get('password/reset/{token?}', 'Auth\PasswordController@showResetForm');
