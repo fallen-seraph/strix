77dedba52973dc51b4fc7d0b18fffadf928851f0
Aaron <fallenseriphan@gmail.com>
2016-06-14 12:30:47 -0600

    Create registerController.php

---
+++ app/Http/Controllers/registerController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\account_information;
+use App\User;
+use Schema;
+use Auth;
+use Illuminate\Database\Schema\Blueprint;
+
+use App\Http\Requests;
+
+class registerController extends Controller
+{
+    public function register(){
+        return view('register');
+    }
+    public function newAccount(Request $request, account_information $account, User $user){
+        // $this->validate($request, [
+        // 	'username' => 'required|unique:strixdb.users,username',
+        // 	'password' => 'required|min:8|confirmed',
+        // 	'password_confirmation' => 'required|min:8',
+        // 	'email' => 'required|email|unique:strixdb.users,email'
+        // ]);
+        $account->create([
+            'email' => $request->email,
+            'first_name' => $request->fname,
+            'last_name' => $request->lname,
+            'account_type' => $request->account_type,
+            'service' => 0,
+            'company' => $request->company,
+            'phone' => $request->phone,
+            'address_one' => $request->addr_one,
+            'address_two' => $request->addr_two,
+            'city' => $request->city,
+            'state' => $request->state,
+            'zip' => $request->zip,
+            'country' => $request->country
+        ]);
+        $accountId = account_information::where('email', $request->email)->value('account_id');
+        $user->create([
+            'account_id' => $accountId,
+            'username' => $request->user,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+        Schema::connection('accountLogs')->create($accountId, function (Blueprint $table) {
+            $table->increments('log_num');
+            $table->string('type');
+            $table->string('value');
+            $table->timestamps();
+        });
+        return view('monitoring');
+    }
+}
