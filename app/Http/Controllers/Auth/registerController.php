613ad28cda67eba900bfddb402e6f700c0e5204b
Aaron <fallenseriphan@gmail.com>
2016-06-14 12:44:05 -0600

    Update registerController.php

--- app/Http/Controllers/Auth/registerController.php
+++ app/Http/Controllers/Auth/registerController.php
@@ -13,16 +13,17 @@ use App\Http\Requests;
 
 class registerController extends Controller
 {
-    public function register(){
+    public function showForm(){
         return view('register');
     }
-    public function newAccount(Request $request, account_information $account, User $user){
+    public function register(Request $request, account_information $account, User $user){
         // $this->validate($request, [
-        // 	'username' => 'required|unique:strixdb.users,username',
+        // 	'username' => 'required|unique:mysql.users,username',
         // 	'password' => 'required|min:8|confirmed',
         // 	'password_confirmation' => 'required|min:8',
-        // 	'email' => 'required|email|unique:strixdb.users,email'
+        // 	'email' => 'required|email|unique:mysql.users,email'
         // ]);
+        
         $account->create([
             'email' => $request->email,
             'first_name' => $request->fname,
@@ -38,19 +39,23 @@ class registerController extends Controller
             'zip' => $request->zip,
             'country' => $request->country
         ]);
+        
         $accountId = account_information::where('email', $request->email)->value('account_id');
+        
         $user->create([
             'account_id' => $accountId,
             'username' => $request->user,
             'email' => $request->email,
             'password' => bcrypt($request->password)
         ]);
+        
         Schema::connection('accountLogs')->create($accountId, function (Blueprint $table) {
             $table->increments('log_num');
             $table->string('type');
             $table->string('value');
             $table->timestamps();
         });
+        
         return view('monitoring');
     }
 }
