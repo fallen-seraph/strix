7e64bdd34cb492b2025443aeff997808b057c725
Aaron <fallenseriphan@gmail.com>
2016-06-14 15:27:29 -0600

    Create monitoring.blade.php

---
+++ resources/views/monitoring.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+
+@section('header')
+    <title>Strix Monitoring</title>
+@stop
+
+@section('content')
+    <a href="monitoring">Monitoring Home</a>
+    <a href="monitoring/users">Users</a>
+    <a href="monitoring/contacts">Contacts</a>
+    <a href="monitoring/contactgroups">Contact Groups</a>
+    <a href="monitoring/hosts">Hosts</a>
+    <a href="monitoring/settings">Settings</a>
+@stop
