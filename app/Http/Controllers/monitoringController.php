7e72121ebc58b40c89cd92a14f9ccb4eec0b5dcb
Aaron <fallenseriphan@gmail.com>
2016-06-15 01:08:06 -0600

    removing the App

--- app/Http/Controllers/monitoringController.php
+++ app/Http/Controllers/monitoringController.php
@@ -3,8 +3,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests;
-use App\User;
-use App\account_information;
+use User;
+use account_information;
 
 class monitoringController extends Controller
 {
