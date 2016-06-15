22c7c537d9cff4738020bf027ccda844ab552356
Aaron <fallenseriphan@gmail.com>
2016-06-15 17:39:51 -0600

    Update monitoringController.php

--- app/Http/Controllers/monitoringController.php
+++ app/Http/Controllers/monitoringController.php
@@ -51,8 +51,9 @@ class monitoringController extends Controller
 	$request->contact_name = Auth::user()->account_id . "_" . $request->alias;
 		
         $this->validate($request, [
+        	'alias' => 'required|min:3'
         	'contact_name' => 'required|unique:nagidb.contacts,contact_name',
-			'email' => 'required|email',
+		'email' => 'required|email',
         ]);
 		
 	$contact->create([
