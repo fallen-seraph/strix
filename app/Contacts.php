790972b559980f166ee54c4705344036ceff442a
Aaron <fallenseriphan@gmail.com>
2016-06-15 17:13:03 -0600

    Update Contacts.php

--- app/Contacts.php
+++ app/Contacts.php
@@ -8,5 +8,5 @@ class Contacts extends Model
 {
     protected $connection = 'nagidb';
     protected $table = 'nagios_contact';
-    protected $fillable = ['account_id', 'contact_name', 'alias', 'contact_groups', 'email', 'phone', misc', 'receive'];
+    protected $fillable = ['account_id', 'contact_name', 'alias', 'contact_groups', 'email', 'phone', 'misc', 'receive'];
 }
