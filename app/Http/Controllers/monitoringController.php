e5b5391d34d3389c65e8c8d3442b8081653d7e1e
Aaron <fallenseriphan@gmail.com>
2016-06-15 01:07:07 -0600

    dd $accountId

--- app/Http/Controllers/monitoringController.php
+++ app/Http/Controllers/monitoringController.php
@@ -17,7 +17,7 @@ class monitoringController extends Controller
     public function users(){
         $accountId = account_information::where('email', 'Auth::user()->email')->value('account_id');
         $users = User::where('account_id', $accountId)->lists('email');
-        dd($users);
+        dd($accountId);
         return view('monitoring.users')->with('users', $users);//, compact('users'));
     }
     public function contacts(){
