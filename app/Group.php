f9ab230b65ce85f0fcc28d0afe156988cd8f700c
Aaron <fallenseriphan@gmail.com>
2016-06-16 00:03:12 -0600

    updating group model

--- app/Group.php
+++ app/Group.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
-    //
+    protected $connection = 'nagidb';
+    protected $table = 'nagios_contact_group';
+    protected $fillable = ['account_id', 'group_name', 'alias', 'members'];
 }
