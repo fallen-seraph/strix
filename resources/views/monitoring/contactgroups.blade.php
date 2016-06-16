05b3c6154b6d4e1c8a923fbcadf67dfc83370d63
Aaron <fallenseriphan@gmail.com>
2016-06-16 17:28:29 -0600

    Update contactgroups.blade.php

--- resources/views/monitoring/contactgroups.blade.php
+++ resources/views/monitoring/contactgroups.blade.php
@@ -50,8 +50,58 @@
                             </div>
                         </form>
                     </div>
-
                     
+                    @if($users->first())
+                        <div class="panel-heading">Add User to Group</div>
+                        <div class="panel-body">
+
+                            <form class="form-horizontal" role="form" method="POST" action="{{ url('/monitoring/group/user') }}">
+                                {{ csrf_field() }}
+
+                                <div class="form-group{{ $errors->has('group') ? ' has-error' : '' }}">
+                                    <label for="group" class="col-md-4 control-label">Group</label>
+
+                                    <div class="col-md-6">
+                                        <select id="group" class="form-control" name="group" value="{{ old('group') }}">
+                                            @foreach($groups as $group)
+                                                <option>{{ $group->alias }}</option>
+                                            @endforeach
+                                        </select>
+                                        @if ($errors->has('group'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('group') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group{{ $errors->has('member') ? ' has-error' : '' }}">
+                                    <label for="member" class="col-md-4 control-label">Member</label>
+
+                                    <div class="col-md-6">
+                                        <select id="member" class="form-control" name="member" value="{{ old('member') }}">
+                                            @foreach($users as $user)
+                                                <option>{{ $user }}</option>
+                                            @endforeach
+                                        </select>
+                                        @if ($errors->has('member'))
+                                            <span class="help-block">
+                                            <strong>{{ $errors->first('member') }}</strong>
+                                        </span>
+                                        @endif
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="col-md-6 col-md-offset-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            <i class="fa fa-btn fa-user"></i> Add Member
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
