4e583ec5f301d7e9060f4b51ff1fb6f699758e10
Aaron <fallenseriphan@gmail.com>
2016-06-16 23:03:48 -0600

    adding / to /monitoring

--- resources/views/monitoring/contactgroups.blade.php
+++ resources/views/monitoring/contactgroups.blade.php
@@ -10,7 +10,7 @@
                     <div class="panel-body">
                         <ul>
                             @foreach ($groups as $group)
-                                <li>{{ $group->alias }} | <a href="monitoring/groups/{{ $group->alias }}">Delete</a></li>
+                                <li>{{ $group->alias }} | <a href="/monitoring/groups/{{ $group->alias }}">Delete</a></li>
                                 @if($group->members[0])
                                     <ul>
                                         Members
