0c3454bece03e25013f802a7515674ff81f7e8eb
Aaron <fallenseriphan@gmail.com>
2016-06-18 13:11:27 -0600

    adjusting spacing

--- resources/views/monitoring/contactgroups.blade.php
+++ resources/views/monitoring/contactgroups.blade.php
@@ -110,12 +110,12 @@
     <script>
         $(document).ready(function($){
             $('#group').change(function(){
-                $.get("{{ url('monitoring/dropdown')}}", { option: $(this).val() },
+                $.get("{{ url('monitoring/dropdown') }}", { option: $(this).val() },
                     function(data) {
                         var member = $('#member');
-                        member.empty();
-                        $.each(data, function(key, value) {
-                            member
+                            member.empty();
+                            $.each(data, function(key, value) {
+                        member
                             .append($("<option></option>")
                             .attr("value",key)
                             .text(value));
