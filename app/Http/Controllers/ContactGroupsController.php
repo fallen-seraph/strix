cc8b5634e4f98bd9d1f00a8f0775d39eeede030b
Aaron <fallenseriphan@gmail.com>
2016-06-18 11:10:54 -0600

    adding response

--- app/Http/Controllers/ContactGroupsController.php
+++ app/Http/Controllers/ContactGroupsController.php
@@ -23,7 +23,7 @@ class ContactGroupsController extends Controller
 
         $users = Contacts::where('account_id', $accountId)->lists('alias');
 
-        return view('monitoring.contactgroups', compact('groups'))->json('users');
+        return view('monitoring.contactgroups', compact('groups'))->response()->json('users');
     }
     public function newGroup(Request $request, Group $group){
         $account_id=Auth::user()->account_id;
