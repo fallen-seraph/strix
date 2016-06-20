6f591439561314ffe545b74bf50f50641b1fd381
Aaron <fallenseriphan@gmail.com>
2016-06-20 15:37:26 -0600

    Update HostController.php

--- app/Http/Controllers/HostController.php
+++ app/Http/Controllers/HostController.php
@@ -21,4 +21,75 @@ class HostController extends Controller
 
         return view('monitoring.hosts', compact('hosts'));
     }
+    public function newHost(Request $request, Host $host){
+        $accountId=Auth::user()->account_id;
+        $hostName=$account_id . "_" . $request->alias;
+        $request->request->add(['host_name' => $hostName]);
+		
+        $this->validate($request, [
+            'group_name' => 'required|unique:nagidb.nagios_host,host_name',
+			'address' => 'required',
+			'contacts' => 'exists:nagidb.nagios_contact,account_id, $accountId',
+			'contact_groups' => 'exists:nagidb.nagios_contact_group,account_id,$accountId',
+			
+        ]);
+		
+        $host->create([
+            'account_id' => $accountId,
+            'host_name' => $hostName,
+            'alias' => $request->alias,
+			'address' => $request->address,
+			'contacts' => $request->contacts,
+			'contact_groups' => $request->contact_groups,
+        ]);
+        
+        return redirect()->action('HostController@hosts');
+    }
+	public function addContacts(Request $request{
+	
+	}
+	public function addService(Request $request, HostService $hostService){
+        $accountId=Auth::user()->account_id;
+		
+        $hostName=$accountId . "_" . $request->host;
+		
+        $host=Host::where('account_id', $accountId)->where('host_name', $hostName)->get();
+		$service=Service::where('service_id', $request->service_id)->select('check_command', 'description');
+		
+        if(strpos($existingServices->services, $newService) !== false){
+            return back()->withErrors(['service' => 'This service is already being monitored on this host.']);
+        }
+		
+		if($existingServices){
+			Host::where('account_id', $accountId)
+				->where('host_name, $hostName)
+				->update([
+					'services' => $existingServices . "," . $request->service,
+				]);
+		} else {
+			Host::where('account_id', $accountId)
+				->where('host_name, $hostName)
+				->update([
+					'services' => $request->service,
+				]);
+		}
+		
+		$hostService->create([
+			'host_id' => $host->host_id,
+			'account_id' => $accountId,
+			'service_id' => $request->service_id,
+			'host_name' => $host->host_name,
+			'service_description' => $service->description,
+			'check_command' => $service->check_command,
+			'arg_one' => $request->arg_one,
+			'arg_two' => $request->arg_two,
+			'arg_three' => $request->arg_three,
+			'arg_four' => $request->arg_four,
+			'arg_five' => $request->arg_five,
+			'contacts' => $host->contacts,
+			'contact_groups' => $host->contact_groups,
+		]);
+		
+        return redirect()->action('HostController@hosts');
+    }
 }
