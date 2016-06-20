4ba3f1c926cfacaa8756237e7be60238bb4cc4b3
Aaron <fallenseriphan@gmail.com>
2016-06-19 22:44:08 -0600

    adjusting logic

--- app/Http/Controllers/ContactGroupsController.php
+++ app/Http/Controllers/ContactGroupsController.php
@@ -30,12 +30,11 @@ class ContactGroupsController extends Controller
         $input = Input::get('option');
 
         $group_contacts = Group::where('account_id', $accountId)->where('alias', $input)->lists('members');
-        //$group_contacts=explode(",", $group_contacts);
 
         $users = Contacts::where('account_id', $accountId)->lists('contact_name');
-        //$users=$users->toArray();
 
-        $availableUsers=array_diff(explode(",", $group_contacts), $users->toArray());
+        $group_contacts=$group_contacts->toArray();
+        $availableUsers=array_diff(explode(",", $group_contacts[0]), $users->toArray());
 
         foreach($availableUsers as &$user){$user=str_replace("1_", "", $user);}
 
