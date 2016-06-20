221c109b7d57f8de85b20f9af1cefec3d7f8022a
Aaron <fallenseriphan@gmail.com>
2016-06-20 13:19:25 -0600

    adding more fields

--- database/migrations/2016_06_20_061544_create_services_table.php
+++ database/migrations/2016_06_20_061544_create_services_table.php
@@ -11,12 +11,18 @@ class CreateServicesTable extends Migration
     public function up()
     {
         Schema::connection('strixProducts')->create('services', function (Blueprint $table) {
-            $table->increments('service_id');
-            $table->string('service_name');
-			$table->string('command_name');
-            $table->integer('available_arguments');
-            $table->timestamps();
-        });
+		$table->increments('service_id');
+		$table->string('service_name');
+		$table->string('command_name');
+		$table->text('description');
+		$table->integer('available_arguments');
+		$table->string('argument_one')->nullable();
+		$table->string('argument_two')->nullable();
+		$table->string('argument_three')->nullable();
+		$table->string('argument_four')->nullable();
+		$table->string('argument_five')->nullable();
+		$table->timestamps();
+	});
     }
     /**
      * Reverse the migrations.
