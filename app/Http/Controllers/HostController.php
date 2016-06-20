f4fe90d4d2fbbe88c81ab6959354a4d73daa4a3d
Aaron <fallenseriphan@gmail.com>
2016-06-20 00:14:19 -0600

    updating Controllers

--- app/Http/Controllers/HostController.php
+++ app/Http/Controllers/HostController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Auth;
+use Host;
 use App\Http\Requests;
 
 class HostController extends Controller
 {
-    //
+    public function __construct(){
+        $this->middleware('auth');
+    }
+    public function hosts(){
+        $accountId=Auth::user()->account_id;
+        $groups = Group::where('account_id', $accountId)->get();
+
+        foreach($groups as $group){
+            $group->members = explode(',', $group->members);
+        }
+
+        return view('monitoring.contactgroups', compact('hosts'));
+    }
 }
