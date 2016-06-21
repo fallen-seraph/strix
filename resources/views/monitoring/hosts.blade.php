2692b83425d370b9e39e74a7f03f3189700db957
Aaron <fallenseriphan@gmail.com>
2016-06-20 23:24:33 -0600

    dropping un-needed fields from add service form

--- resources/views/monitoring/hosts.blade.php
+++ resources/views/monitoring/hosts.blade.php
@@ -136,44 +136,10 @@
                                     @endif
                                 </div>
                             </div>
-                            <div class="form-group{{ $errors->has('contacts') ? ' has-error' : '' }}">
-                                <label for="contacts" class="col-md-4 control-label">contacts</label>
-
-                                <div class="col-md-6">
-                                    <select id="contacts" class="form-control" name="contacts">
-                                        @foreach($contacts as $contact)
-                                            <option>{{ trim($contact, Auth::user()->account_id . "_") }}</option>
-                                        @endforeach
-                                    </select>
-                                    @if ($errors->has('contacts'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('contacts') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group{{ $errors->has('contact_groups') ? ' has-error' : '' }}">
-                                <label for="contact_groups" class="col-md-4 control-label">contact_groups</label>
-
-                                <div class="col-md-6">
-                                    <select id="contact_groups" class="form-control" name="contact_groups">
-                                        @foreach($contact_groups as $groups)
-                                            <option>{{ trim($groups, Auth::user()->account_id . "_") }}</option>
-                                        @endforeach
-                                    </select>
-                                    @if ($errors->has('contact_groups'))
-                                        <span class="help-block">
-                                        <strong>{{ $errors->first('contact_groups') }}</strong>
-                                    </span>
-                                    @endif
-                                </div>
-                            </div>
-
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
                                     <button type="submit" class="btn btn-primary">
-                                        <i class="fa fa-btn fa-user"></i> New Host
+                                        <i class="fa fa-btn fa-user"></i> Add Service
                                     </button>
                                 </div>
                             </div>
