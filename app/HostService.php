35a439a4433b28ea91c2b24dce1de2827a357ccd
Aaron <fallenseriphan@gmail.com>
2016-06-20 23:31:31 -0600

    updating hostservices model

--- app/HostService.php
+++ app/HostService.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class HostService extends Model
 {
-    //
+    protected $connection = 'nagidb';
+    protected $table = 'nagios_host_services';
+    protected $fillable = ['host_id', 'account_id', 'service_id', 'host_name', 'service_description', 'check_command', 'contacts', 'contact_groups'];
 }
