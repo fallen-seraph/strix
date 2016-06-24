e333d12ab0fe5d8f397e0758108e016bfbc32e17
Aaron <fallenseriphan@gmail.com>
2016-06-24 11:51:15 -0600

    Update UpdatesController.php

--- app/Http/Controllers/UpdatesController.php
+++ app/Http/Controllers/UpdatesController.php
@@ -13,8 +13,9 @@ class UpdatesController extends Controller
     public function __construct(){
         $this->middleware('auth');
     }
-    public function contact($contact_id){
-        $contact=Contacts::where('contact_id', $contact_id)->first();
+    public function contact($alias){
+        $accountId=Auth::user()->account_id;
+        $contact=Contacts::where('account_id', $accountId)->where('alias', $alias)->first();
 
         return view('monitoring.updates.updateContacts', compact('contact'));
     }
@@ -28,6 +29,23 @@ class UpdatesController extends Controller
             'misc' => $request->misc,
         ]);
 
+        return redirect()->action('ContactsController@contacts');
+    }
+    public function contact($group){
+        $accountId=Auth::user()->account_id;
+        $group=Group::where('account_id', $accountid)->where('alias', $group)->first();
+        
+        return view('monitoring.updates.updateGroups', compact('group'));
+    }
+    public function updateContact(Request $request){
+        $accountId=Auth::user()->account_id;
+        Contacts::where('account_id', $accountId)->where('contact_id', $request->contact_id)->update([
+            'contact_name' => $accountId . "_" . $request->alias,
+            'alias' => $request->alias,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'misc' => $request->misc,
+        ]);
         return redirect()->action('ContactsController@contacts');
     }
 }
