0eb3f093fd0f4dde9757242fe39f64609a1d38b2
Aaron <fallenseriphan@gmail.com>
2016-06-23 22:31:31 -0600

    image file to big, commenting for now

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="img/logo.png" alt="We watch it so you don't have to!">
+                    Server Strix<!--<img src="img/logo.png" alt="We watch it so you don't have to!">-->
                 </a>
             </div>
 
