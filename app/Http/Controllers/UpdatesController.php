adf737ad929c26272d8b269d23f1d48782eb6316
Aaron <fallenseriphan@gmail.com>
2016-06-25 00:01:43 -0600

    full path to input

--- app/Http/Controllers/UpdatesController.php
+++ app/Http/Controllers/UpdatesController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 use Auth;
-use Input;
 use App\Contacts;
 use App\Group;
 use App\Http\Requests;
