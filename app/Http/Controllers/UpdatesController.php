c62b8cdd2ad7a5896f69f4cac581e5b81a918512
Aaron <fallenseriphan@gmail.com>
2016-06-24 23:51:10 -0600

    major logic update

--- app/Http/Controllers/UpdatesController.php
+++ app/Http/Controllers/UpdatesController.php
@@ -51,13 +51,72 @@ class UpdatesController extends Controller
     }
     public function updateGroup(Request $request){
         $accountId=Auth::user()->account_id;
-        Contacts::where('account_id', $accountId)->where('contact_id', $request->contact_id)->update([
-            'contact_name' => $accountId . "_" . $request->alias,
-            'alias' => $request->alias,
-            'email' => $request->email,
-            'phone' => $request->phone,
-            'misc' => $request->misc,
-        ]);
-        return redirect()->action('ContactsController@contacts');
+
+        if(Input::get('nameChange')){
+
+            Group::where('account_id', $accountId)
+                ->where('group_id', $request->group_id)
+                ->update([
+                    'group_name' => $accountId . "_" . $request->alias,
+                    'alias' => $request->alias,
+                ]);
+
+        } elseif(Input::get('add')) {
+            $group=Group::where('account_id', $accountId)->where('group_id', $request->group_id)->first();
+            $contact=Contacts::where('account_id', $accountId)->where('alias', $request->availableMembers)->value('contact_groups');
+
+            if($group->members) {
+                Group::where('account_id', $accountId)
+                    ->where('group_id', $request->group_id)
+                    ->update([
+                        'members' => $group->members . "," . $request->availableMembers,
+                    ]);
+            } else {
+                Group::where('account_id', $accountId)
+                    ->where('group_id', $request->group_id)
+                    ->update([
+                        'members' => $request->availableMembers,
+                    ]);
+            }
+
+            if($contact){
+                Contacts::where('account_id', $accountId)
+                    ->where('alias', $request->availableMembers)
+                    ->update([
+                        'contact_groups' => $contact . "," . $group->group_name
+                    ]);
+            } else {
+                Contacts::where('account_id', $accountId)
+                    ->where('alias', $request->availableMembers)
+                    ->update([
+                        'contact_groups' => $group->group_name
+                    ]);
+            }
+
+        } elseif(Input::get('remove')) {
+            $removedContact=str_replace("*", "", $request->availableMembers);
+            $group=Group::where('account_id', $accountId)->where('group_id', $request->group_id)->first();
+            $contact=Contacts::where('account_id', $accountId)->where('alias', $request->availableMembers)->value('contact_groups');
+
+            if(str_pos(",", $group->members) !== false) {
+                Group::where('account_id', $accountId)->where('group_id', $request->group_id)->update([
+                    'members' => str_replace($removedContact . ",", "", $group->members),
+                ]);
+            } else {
+                Group::where('account_id', $accountId)->where('group_id', $request->group_id)->update([
+                    'members' => str_replace($removedContact, "", $group->members),
+                ]);
+            }
+            if(str_pos(",", $contact) !== false){
+                Contacts::where('account_id', $accountId)->where('alias', $request->availableMembers)->update([
+                    'contact_groups' => str_replace($group->group_name . ",", "", $contact),
+                ]);
+            } else {
+                Contacts::where('account_id', $accountId)->where('alias', $request->availableMembers)->update([
+                    'contact_groups' => str_replace($group->group_name, "", $contact),
+                ]);
+            }
+        }
+        return redirect()->action('UpdatesController@group');
     }
 }
--- resources/views/monitoring/updates/updateGroups.blade.php
+++ resources/views/monitoring/updates/updateGroups.blade.php
@@ -13,13 +13,43 @@
                             {{ method_field('PATCH') }}
 
                             <input id="group_id" type="hidden" name="group_id" value="{{ $group->group_id }}">
-                            
+
+                            <div class="form-group{{ $errors->has('groupName') ? ' has-error' : '' }}">
+                                <label for="groupName" class="col-md-4 control-label">Group Name</label>
+
+                                <div class="col-md-6">
+                                    <input id="groupName" class="form-control" name="groupName" value="{{ $group->alias }}">
+                                    @if ($errors->has('groupName'))
+                                        <span class="help-block">
+                                        <strong>{{ $errors->first('groupName') }}</strong>
+                                    </span>
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <button type="submit" class="btn btn-primary" id="nameChange">
+                                        <i class="fa fa-btn fa-user"></i> Change Group Name
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="panel-body">
+
+                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/monitoring/groups/update') }}">
+                            {{ csrf_field() }}
+                            {{ method_field('PATCH') }}
+
+                            <p class="col-md-6">Contact* = Member of {{ $group->alias }}</p>
+
+                            <input id="group_id" type="hidden" name="group_id" value="{{ $group->group_id }}">
+
                             <div class="form-group{{ $errors->has('availableMembers') ? ' has-error' : '' }}">
                                 <label for="availableMembers" class="col-md-4 control-label">Contact List</label>
 
 
                                 <div class="col-md-6">
-                                    <p>Contact* = Member of {{ $group->alias }}</p>
                                     <select id="availableMembers" class="form-control" name="availableMembers">
                                         <option selected disabled>Choose a contact</option>
                                         @foreach($availableContacts as $contact)
