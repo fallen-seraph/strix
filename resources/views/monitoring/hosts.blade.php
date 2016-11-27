2d57aac0cad21711d9a114dfef4fb71f4cd15aa5
Aaron <aladd@10.239.195.42>
2016-11-27 13:34:28 -0700

    adjusting opening list

--- resources/views/monitoring/hosts.blade.php
+++ resources/views/monitoring/hosts.blade.php
@@ -10,37 +10,28 @@
                     <div class="panel-body">
                         @foreach ($hosts as $host)
                             <ul>
-                                Hostname:
-                                <ul>
-                                    <li>{{ $host->host_name }} | <a href="/monitoring/update/hosts/{{ $host->host_name }}">Update</a> | <a href="/monitoring/hosts/delete/{{ $host->host_name }}">Delete</a></li>
-                                </ul>
-                                IP Address:
-                                    <ul>
-                                        <li>{{ $host->address }}</li>
-                                        @if(isset($host->services))
-                                            Services:
-                                            <ul>
-                                                @foreach($host->services as $service)
-                                                    <li>{{ $service }}</li>
-                                                @endforeach
-                                            </ul>
-                                        @endif
-                                    </ul>
+                                {{ $host->host_name }} | <a href="/monitoring/update/hosts/{{ $host->host_name }}">Update</a> | <a href="/monitoring/hosts/delete/{{ $host->host_name }}">Delete</a></li>
+                                <li>IP Address : {{ $host->address }}</li>
+                                @if(isset($host->services))
+                                    <li>Services :
+                                    @foreach($host->services as $service)
+                                        <li>{{ $service }}</li>
+                                    @endforeach
+                                    </il>
+                                @endif
                                 @if(isset($host->contacts))
-                                    Contacts
-                                    <ul>
-                                        @foreach($host->contacts as $contact)
-                                            <li>{{ $contact }}</li>
-                                        @endforeach
-                                    </ul>
+                                    <li>Contacts
+                                    @foreach($host->contacts as $contact)
+                                        <li>{{ $contact }}</li>
+                                    @endforeach
+                                    </li>
                                 @endif
                                 @if(isset($host->contact_groups))
-                                    Contact Groups
-                                    <ul>
-                                        @foreach($host->contact_groups as $groups)
-                                            <li>{{ $groups }}</li>
-                                        @endforeach
-                                    </ul>
+                                    <li>Contact Groups
+                                    @foreach($host->contact_groups as $groups)
+                                        <li>{{ $groups }}</li>
+                                    @endforeach
+                                    </li>
                                 @endif
                             </ul>
                         @endforeach
